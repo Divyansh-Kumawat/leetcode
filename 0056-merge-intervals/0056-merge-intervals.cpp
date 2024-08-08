26dd65450b04f594fb07f6cd93153d0df3978f0d
Divyansh-Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2024-08-08 23:21:09 +0530

    Time: 360 ms (5.09%), Space: 22.5 MB (92.71%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,9 +6,7 @@ public:
         int i = 0;
         while (i < intervals.size() - 1) {
             if (intervals[i][1] >= intervals[i+1][0]) {
-                // Merge intervals
                 intervals[i][1] = max(intervals[i][1], intervals[i+1][1]);
-                // Erase the merged interval
                 intervals.erase(intervals.begin() + i + 1);
             } else {
                 i++;
