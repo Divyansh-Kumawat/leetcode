0fd224c651cbca4d4e37f2e3b57a8e86cc3f14d6
Divyansh-Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2024-10-25 21:39:01 +0530

    Time: 2 ms (84.84%), Space: 12.5 MB (25.03%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-         vector<int> son(n); 
-        for (int i = 0; i < n; ++i) {
-          auto it = find(nums2.begin(), nums2.end(), nums1[i]); 
-          bool found = false;
-          for (auto j = it + 1; j != nums2.end(); ++j) {
-            if (*j > nums1[i]) {
-              son[i] = *j;
-              found = true;break;}}
-            if (!found) son[i] = -1;}return son;}};
+        vector<int>nge;
+        stack<int> stk;
+        int n2=nums2.size();
+        int n1=nums1.size();
+        unordered_map<int,int> mpp;
+        
+        for(int i=n2-1;i>=0;i--){
+            while(!stk.empty() && nums2[i]>stk.top()){
+                stk.pop();
+            }
+            if(stk.empty()) mpp[nums2[i]]=-1;
+            else mpp[nums2[i]]=stk.top();
+            stk.push(nums2[i]);
+        }
+        
+        
+        for(int i=0;i<n1;i++){
+            nge.push_back(mpp[nums1[i]]);
+        }
+        return nge;
+    }
+};
   
