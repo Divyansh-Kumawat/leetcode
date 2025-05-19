020c2408329977878d776bf69674ff833c3b414e
Divyansh-Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2025-05-19 16:40:58 +0530

    Time: 0 ms (100%), Space: 12.1 MB (20.36%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -5,18 +5,13 @@ public:
         string first= strs[0];
         string second= strs[strs.size()-1];
         string finalstr="";
-        bool x= true;
-        for(int i=0;i<first.length();i++){
-            if(first[i]==second[i]){
-                finalstr+=first[i];
-            }
-            else{
-                break;
-            }
+        int i=0;
+        if(strs.size()==1) return strs[0];
+        if(strs[0]=="") return "";
+         while (i < first.size() && i < second.size() && first[i] == second[i]) {
+            finalstr += first[i];
+            i++;
         }
         return finalstr;
-        
-
-    
     }
 };
