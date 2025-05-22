5c714daeeccb5a51c2de09bf6f50fab5a22dc2c5
Divyansh-Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2025-05-22 15:53:59 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.42%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        double ans=1.0;
-        long long nn=n;
-        if(nn<0) nn=-1*nn;
-        while(nn){
-            if(nn%2){
-                ans=ans*x;
-                nn=nn-1;                
-            }
-            else{
-                x=x*x;
-                nn=nn/2;
-            }
-        }
-        if(n<0)
-        {
-            ans=(double)1/(double)ans;
-            
-        }
-        return ans;
+    double fastPow(double x, long long n) {
+        if (n == 0) return 1.0;
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0)
+            return half * half;
+        else
+            return half * half * x;
     }
+
+
+    double myPow(double x, int n) {
+        long long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        return fastPow(x,n);
+    }
+
 };
