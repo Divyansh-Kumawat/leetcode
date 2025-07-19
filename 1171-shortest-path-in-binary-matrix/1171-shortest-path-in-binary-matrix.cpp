3068723e405f99e30d944b5e2dbb946d0eb1436f
Divyansh-Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2025-07-19 21:53:11 +0530

    Time: 36 ms (28.18%), Space: 23.3 MB (96.85%) - LeetHub

---
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        priority_queue<
+            pair<int, pair<int, int>>,
+            vector<pair<int, pair<int, int>>>,
+            greater<pair<int, pair<int, int>>>
+        > pq; 
+        vector<vector<int>> dist(n,vector<int>(m,1e9));
+        if(grid[0][0]==0){
+            dist[0][0]=1;
+            pq.push({1,{0,0}});
+        }   
+        else return -1;
+        int drow[]={0,1,0,-1,-1,1,1,-1};
+        int dcol[]={1,0,-1,0,-1,1,-1,1};
+        while(!pq.empty()){
+            int dis=pq.top().first;
+            int r=pq.top().second.first;
+            int c=pq.top().second.second;
+            pq.pop();
+            for(int i=0;i<8;i++){
+                int row=r+drow[i];
+                int col=c+dcol[i];
+                if(col>=0 && row>=0 && row<n && col<m){
+                    if(grid[row][col]==0 && dist[row][col]>dis+1){
+                        dist[row][col]=dis+1;
+                        pq.push({dis + 1, {row, col}});
+                    }
+                }
+            }
+        }
+        if(dist[n-1][m-1]==1e9) return -1;
+        return dist[n-1][m-1];
+    }
+};
