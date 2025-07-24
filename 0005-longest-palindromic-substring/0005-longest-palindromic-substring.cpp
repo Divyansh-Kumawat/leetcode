4a3b730b4dd03ffba846b9af87544429bd08275a
Divyansh-Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2025-07-24 23:35:50 +0530

    Time: 960 ms (8.23%), Space: 471.4 MB (5%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
-    bool function(string &s,int i,int j,vector<vector<int>>&dp)
-    {
-        if(i>=j)
-        {
-            return true;
-        }
-        if(dp[i][j] != -1)
-        {
-            return dp[i][j];
-        }
-        if(s[i] == s[j])
-        {
-            return dp[i][j] = function(s,i+1,j-1,dp);
-        }
-        return dp[i][j] = false;
-    }
     string longestPalindrome(string s) {
-        int size = s.length();
-        string temp = "";
-        int maxLength = INT_MIN;
-        vector<vector<int>>dp(size,vector<int>(size,-1));
-        for(int i = 0;i<size;i++)
-        {
-            for(int j = i;j<size;j++)
-            {
-                if(function(s,i,j,dp))
-                {
-                    if(maxLength < (j-i+1))
-                    {
-                        maxLength = (j-i+1);
-                        temp = s.substr(i,maxLength);
-                    }
-                }
+        int n=s.size();
+        string str="";
+        for(int i=0;i<n;i++){
+            int x=i;
+            int y=i;
+            while(x>=0&& y<n&& s[x]==s[y]){
+                
+                string pal = s.substr(x, y-x+1 );
+                if(pal.size()>str.size()){
+                    str=pal;
+                }x--;y++;
+                
+            }
+            x=i;
+            y=i+1;
+            while(x>=0 && y<n && s[x]==s[y]){
+                
+                
+                string pal = s.substr(x, y-x+1);
+                if(pal.size()>str.size()){
+                    str=pal;
+                }x--;y++;
+                
             }
         }
-        return temp;
+        return str;
     }
 };
