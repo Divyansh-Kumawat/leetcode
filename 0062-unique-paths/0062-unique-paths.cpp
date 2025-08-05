1f930987160c0be78ed9ea77b53169f88d69f1cc
Divyansh-Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2025-08-05 23:20:06 +0530

    Time: 0 ms (100%), Space: 9.4 MB (8.68%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -6,11 +6,25 @@ public:
         if(dp[m][n]!=-1) return dp[m][n];
         int left= paths(m-1,n,dp);
         int right = paths(m,n-1,dp);
-        return dp[m][n]=left+right;
+        dp[m][n]=left+right;
+        return dp[m][n];
         
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n, -1));
-        return paths(m-1,n-1,dp);
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1; // Base case, already set
+                } else{
+                    int left= (i>0)?dp[i-1][j]:0;
+                    int right=(j>0)?dp[i][j-1]:0;
+                    dp[i][j]=left+right;
+                }
+                
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
