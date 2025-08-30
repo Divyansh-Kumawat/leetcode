b59d8049a4a5eed40e4ab18d9edf54eeb3eac0d7
Divyansh-Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2025-08-30 16:35:19 +0530

    Time: 5 ms (48.84%), Space: 15 MB (13.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-        vector<int> arr;
-        for(int i=0;i<nums.size()-1;i++){
-            for(int j=i+1;j<nums.size();j++)
-            if(nums[i]+nums[j]==target){
-                arr.push_back(i);
-                arr.push_back(j);
-               
+        int n=nums.size();
+        map<int,int> mpp;
+        int sum=0;
+        mpp[nums[0]]=0;
+        vector<int> ans;
+        for(int i=1;i<n;i++){
+            int rem=target-nums[i];
+            if(mpp.find(rem)!=mpp.end()){
+                ans.push_back(mpp[rem]);
+                ans.push_back(i);
+                break;
             }
+            mpp[nums[i]]=i;
+
         }
-        return  arr ;
-//         sort(nums.begin(),nums.end());
-//         int n=nums.size();
-//         int left=0;
-//         int right=n-1;
-//         vector<int> arr;
-//         while(left<right){
-//             int sum=nums[left]+nums[right];
-//             if(sum==target){
-//                 arr.push_back(left);
-//                 arr.push_back(right);
-//             }
-//             else if(sum<target){
-//                 left++;
-//             }
-//             else{
-//                 right--;
-//             }
-//         }
-        
-//         return arr;
+        return ans;
     }
 };
