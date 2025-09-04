e7f3ccf88ae36599a961860195bd7241b36348fc
Divyansh-Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2025-09-04 22:01:52 +0530

    Time: 0 ms (100%), Space: 17.6 MB (17.62%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,77 +1,32 @@
 class Solution {
 public:
-
-vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> result = {-1, -1};
-        int left = binarySearch(nums, target, true);
-        int right = binarySearch(nums, target, false);
-        result[0] = left;
-        result[1] = right;
-        return result;        
-    }
-
-    int binarySearch(vector<int>& nums, int target, bool isSearchingLeft) {
-        int left = 0;
-        int right = nums.size() - 1;
-        int idx = -1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                idx = mid;
-                if (isSearchingLeft) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans(2,-1);
+        int low=0;
+        int n=nums.size();
+        int high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target){
+                int x=mid;
+                int y=mid;
+                while(x>0&& nums[x-1]==target){
+                    x--;
                 }
+                while(y<n-1 &&nums[y+1]==target){
+                    y++;
+                }
+                ans[0]=x;
+                ans[1]=y;
+            }
+            if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+
             }
         }
-        
-        return idx;
-//     int func(vector<int>& nums,vector<int>& arr,int low,int high,int target){
-//         if(low>high){
-//             return;
-//         }
-        
-//     }
-    // vector<int> searchRange(vector<int>& nums, int target) {
-        // int n=nums.size();
-        // vector<int> arr;
-        // int low = 0;
-        // int high=n-1;
-        // func(nums,arr,low,high,target);
-        
-        
-//         int count=0;
-//         while(low<=high){
-//             int mid=(low+high)/2;
-//             if(nums[mid]==target){
-//                 arr.push_back(mid);
-//                 int count1 =mid+1;
-//                 int count2 =mid-1;
-//                 // while(nums[count2]==target ){
-//                 //     arr.push_back(count2);
-//                 //     count--;
-//                 // }
-//                 return arr;
-//             }
-//             else if(nums[mid]>target){
-//                 high=mid-1;
-//             }
-//             else{
-//                 low=mid+1;
-//             }
-//         }
-//         if(arr.empty()){
-//             arr.push_back(-1);
-//             arr.push_back(-1);
-
-//         }
-//         return arr;
+        return ans;
     }
 };
