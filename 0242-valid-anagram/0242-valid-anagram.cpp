7dc1436a589eadb46b0ef921dbc93c472182b60a
Divyansh-Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2025-09-06 15:50:57 +0530

    Time: 7 ms (20.33%), Space: 9.7 MB (48.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,43 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int> mpps;
-        map<char,int> mppt;
+        if (s.size() != t.size()) return false;
+        map<char,int> mpp;
         for(char c:s){
-            if((mpps.find(c)!=mpps.end())){
-                mpps[c]++;
-            }
-            else{
-                mpps[c]=1;
-            }
+            mpp[c]++;
         }
         for(char c:t){
-            if((mppt.find(c)!=mppt.end())){
-                mppt[c]++;
-            }
-            else{
-                mppt[c]=1;
-            }
+            mpp[c]--;
+            if(mpp[c]==0) mpp.erase(c);
         }
-        int n=mpps.size();
-        int m=mppt.size();
-        if(n!=m) return false;
-        int cnt=0;
-
-        for(const auto& pair: mpps){
-            if(mppt.find(pair.first)!=mppt.end()){
-                if(pair.second==mppt[pair.first]){
-                    cnt++;
-                }
-                else{
-                    return false;
-                }
-            }
-            else{
-                return false;
-            }
-        }
-        if(cnt==n)return true;
-        return false;
+        return mpp.empty();
+            
     }
 };
