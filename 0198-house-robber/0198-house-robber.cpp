f525dc4c62395c7507ea41923ebe645446e03a39
Divyansh-Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2025-10-01 17:43:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    int find(vector<int> & nums,int i,vector<int>& dp){
-        if(i==0) return nums[0];
-        if(i<0) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int pick=nums[i]+find(nums,i-2,dp);
-        int notpick=find(nums,i-1,dp);
-        
-        return dp[i]=max(pick,notpick);
-
+    int maxmoney(vector<int> & nums,int n,vector<int> dp){
+        if(n==0 || n==1) return nums[n];
+        if(dp[n]!=-1) return dp[n];
+        int notrob=maxmoney(nums,n-1,dp);
+        int rob;
+        if(n>1) rob=maxmoney(nums,n-2,dp)+nums[n];
+        return dp[n]=max(rob,notrob);
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,-1);
-        return find(nums,n-1,dp);
+        vector<int> dp(n+1,-1);
+        return maxmoney(nums,n-1,dp);
     }
 };
