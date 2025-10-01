e7a75a3b98f5d15abdc6410a6a29f59cb1e1acae
Divyansh-Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2025-10-01 17:48:34 +0530

    Time: 0 ms (100%), Space: 10.7 MB (46.62%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    int maxmoney(vector<int> & nums,int n,vector<int> dp){
-        if(n==0 || n==1) return nums[n];
+    int maxmoney(vector<int> & nums,int n,vector<int>& dp){
+        if(n==0) return nums[0];
+        if(n<0) return 0;
         if(dp[n]!=-1) return dp[n];
+        int rob=maxmoney(nums,n-2,dp)+nums[n];
         int notrob=maxmoney(nums,n-1,dp);
-        int rob;
-        if(n>1) rob=maxmoney(nums,n-2,dp)+nums[n];
+        
         return dp[n]=max(rob,notrob);
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n+1,-1);
+        vector<int> dp(n,-1);
         return maxmoney(nums,n-1,dp);
     }
 };
