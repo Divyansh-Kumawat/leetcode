e1577453fe36a78ced15f38046bb4fbbf621d2a6
Divyansh-Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2025-10-03 22:31:00 +0530

    Time: 2 ms (18.71%), Space: 9.5 MB (8.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
-    int paths(int m,int n,vector<vector<int>>& dp){
-        if(n==0 && m==0) return 1;
-        if(n<0 || m<0) return 0;
+    int ways(int m,int n,vector<vector<int>>& dp){
+        if(m==0 && n==0) return 1;
+
+        if(m<0 || n<0) return 0;
         if(dp[m][n]!=-1) return dp[m][n];
-        int left= paths(m-1,n,dp);
-        int right = paths(m,n-1,dp);
-        dp[m][n]=left+right;
-        return dp[m][n];
-        
+        int left=ways(m,n-1,dp);
+        int up=ways(m-1,n,dp);
+        return dp[m][n]=left+up;
+
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n, -1));
-        dp[0][0]=1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if (i == 0 && j == 0) {
-                    dp[i][j] = 1; // Base case, already set
-                } else{
-                    int left= (i>0)?dp[i-1][j]:0;
-                    int right=(j>0)?dp[i][j-1]:0;
-                    dp[i][j]=left+right;
-                }
-                
-            }
-        }
-        return dp[m-1][n-1];
+        return ways(m-1,n-1,dp);
     }
 };
