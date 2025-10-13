4809e0f09cbb22f1b1313253bb2486733ebeccd4
Divyansh-Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2025-10-13 18:19:33 +0530

    Time: 397 ms (46.57%), Space: 211.4 MB (50.68%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
-    int maxprofit(int ind,int buy,int n,vector<int>& prices,int cnt,vector<vector<vector<int>>> & dp){
-        if(cnt==0) return 0;
-        if(ind==n) return 0;
-        if (dp[ind][buy][cnt] != -1) return dp[ind][buy][cnt];
-        int profit=0;
-        if(!buy){
-            profit= max(-prices[ind]+maxprofit(ind+1,1,n,prices,cnt,dp),
-            maxprofit(ind+1,0,n,prices,cnt,dp));
-        }
-        else{
-            profit= max(prices[ind]+maxprofit(ind+1,0,n,prices,cnt-1,dp),
-            maxprofit(ind+1,1,n,prices,cnt,dp));
-        }
-        return dp[ind][buy][cnt] =profit;
-    }
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
-        return maxprofit(0,0,n,prices,2,dp);
+        int n = prices.size();
+        // dp[ind][buy][cnt]
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+        // Base case already initialized to 0 (no profit if cnt=0 or ind=n)
+
+        // Fill table from back to front
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cnt = 1; cnt <= 2; cnt++) {
+                    if (buy == 0) {
+                        dp[ind][buy][cnt] = max(
+                            -prices[ind] + dp[ind + 1][1][cnt],
+                            dp[ind + 1][0][cnt]
+                        );
+                    } else {
+                        dp[ind][buy][cnt] = max(
+                            prices[ind] + dp[ind + 1][0][cnt - 1],
+                            dp[ind + 1][1][cnt]
+                        );
+                    }
+                }
+            }
+        }
+
+        // Final answer: starting at day 0, can buy, with 2 transactions left
+        return dp[0][0][2];
     }
 };
