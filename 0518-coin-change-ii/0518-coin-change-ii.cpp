d67df351d098c02c4b7c3c6808c864aef1c26f18
Divyansh Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2025-10-23 22:25:15 +0530

    Time: 40 ms (19.29%), Space: 61.2 MB (9.31%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
-    int value(vector<int>& coins,int ind,int amount,vector<vector<int>>& dp){
-        if(ind==0){
-            return amount% coins[ind]==0;
-        }
-        if(dp[ind][amount]!=-1) return dp[ind][amount];
-        int notpick=value(coins,ind-1,amount,dp);
-        int pick=0;
-        if(coins[ind]<=amount) pick=value(coins,ind,amount-coins[ind],dp);
-        return dp[ind][amount]=pick+notpick;
-    }
+    // int ways(vector<int>& coins,int ind,int amount,vector<vector<int>> &dp){  
+    //     if(ind==0){
+    //        return amount%coins[ind]==0;
+    //     }
+    //     if(dp[ind][amount]!=-1) return dp[ind][amount];
+    //     int notpick=ways(coins,ind-1,amount,dp);
+    //     int pick=0;
+    //     if(amount >=coins[ind]){
+    //         pick=ways(coins,ind,amount-coins[ind],dp);
+    //     }
+    //     return dp[ind][amount]=pick+notpick;
+
+    // }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return value(coins,n-1,amount,dp);
+        const long long MOD = 1e9 + 7;
+        vector<vector<long long>> dp(n,vector<long long>(amount+1,0));
+        for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0) dp[0][i] = 1;
+        }
+        for(int ind=1;ind<n;ind++){
+            for(int target=0;target<=amount;target++){
+                long long notpick=dp[ind-1][target];
+                long long pick=0;
+                if(target>=coins[ind]){
+                    pick=dp[ind][target-coins[ind]];
+                }
+                dp[ind][target]=pick+notpick%MOD;
+            }
+        }
+        return (int) dp[n-1][amount];
+
+        // return ways(coins,n-1,amount,dp);
     }
 };
