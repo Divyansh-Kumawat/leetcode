0586c75eebe7b6cd0ff2df5f874ede4aa0e39dac
Divyansh Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2025-11-10 18:58:11 +0530

    Time: 4 ms (24.19%), Space: 9.3 MB (79.02%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -6,12 +6,10 @@ public:
         if(n!=m) return false;
         char st[256]={0};
         char ts[256]={0};
-
         for(int i=0;i<n;i++){
             char a=s[i];
             char b=t[i];
-            if((st[a] && st[a]!=b) || ts[b] && ts[b]!=a) return false;
-
+            while((st[a] && st[a]!=b) || (ts[b] && ts[b]!=a)) return false;
             st[a]=b;
             ts[b]=a;
         }
