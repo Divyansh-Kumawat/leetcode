bfd837cc56a00ee703c159c1811a989b75ba1f96
Divyansh Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2026-02-28 15:38:43 +0530

    Time: 0 ms (100%), Space: 14.4 MB (58.73%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& board,vector<vector<int>>& vis,int n,int m,int r,int c){
-        vis[r][c]=1;
-        int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
-        for(int i=0;i<4;i++){
-            int col=c+dcol[i];
-            int row=r+drow[i];
-            if(row>=0 && col>=0 && row<n && col<m && board[row][col]=='O' && vis[row][col]==0){
-                dfs(board,vis,n,m,row,col);
+    void dfs(vector<vector<char>>& board,vector<vector<int>>& vis,int i,int j,int n,int m){
+        vis[i][j]=1;
+        int drow[]={0,1,0,-1};
+        int dcol[]={1,0,-1,0};
+        for(int k=0;k<4;k++){
+            int r=i+drow[k];
+            int c=j+dcol[k];
+            if(r>=0 && c>=0 && r<n && c<m && board[r][c]=='O' && !vis[r][c]){
+                dfs(board,vis,r,c,n,m);
             }
         }
+
     }
     void solve(vector<vector<char>>& board) {
         int n=board.size();
@@ -18,22 +19,22 @@ public:
         vector<vector<int>> vis(n,vector<int>(m,0));
         for(int i=0;i<m;i++){
             if(board[0][i]=='O'){
-                dfs(board,vis,n,m,0,i);
+                dfs(board,vis,0,i,n,m);
             }
         }
         for(int i=0;i<m;i++){
             if(board[n-1][i]=='O'){
-                dfs(board,vis,n,m,n-1,i);
+                dfs(board,vis,n-1,i,n,m);
             }
         }
         for(int i=0;i<n;i++){
             if(board[i][0]=='O'){
-                dfs(board,vis,n,m,i,0);
+                dfs(board,vis,i,0,n,m);
             }
         }
         for(int i=0;i<n;i++){
             if(board[i][m-1]=='O'){
-                dfs(board,vis,n,m,i,m-1);
+                dfs(board,vis,i,m-1,n,m);
             }
         }
         for(int i=0;i<n;i++){
@@ -43,6 +44,5 @@ public:
                 }
             }
         }
-        
     }
 };
