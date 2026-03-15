930207734a72406fcf9519f2ad84d305a639c5ce
Divyansh Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2026-03-15 17:19:26 +0530

    Time: 119 ms (44.81%), Space: 9 MB (84.49%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    
-    int beauty(vector<int>& alpha)
-    {
-        int mf=-1;
-        int lf=1e9;
-        for(int i=0;i<26;i++){
-            mf=max(mf,alpha[i]);
-            if(alpha[i]>=1){
-                lf=min(lf,alpha[i]);
-            }
-        }
-        return mf-lf;
-    }
     int beautySum(string s) {
-        int res=0;
-        for(int i=0;i<s.size();i++){
-            vector<int> alpha(26,0);
-            for(int j=i;j<s.size();j++)
-            {
-                alpha[s[j]-'a']++;
-                res=res+beauty(alpha);
+        int n = s.size();
+        int total = 0;
+
+        // Iterate over all starting points
+        for (int i = 0; i < n; i++) {
+            int freq[26] = {0};
+
+            // Expand substring ending at j
+            for (int j = i; j < n; j++) {
+                freq[s[j] - 'a']++;
+
+                int mx = 0, mn = INT_MAX;
+                for (int k = 0; k < 26; k++) {
+                    if (freq[k] > 0) {
+                        mx = max(mx, freq[k]);
+                        mn = min(mn, freq[k]);
+                    }
+                }
+                total += (mx - mn);
             }
         }
-        return res;
+        return total;
     }
 };
