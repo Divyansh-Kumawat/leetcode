aaa1bb28c4be524cc08818c6c6373ff187a37503
Divyansh Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2026-03-16 17:01:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    void func(int n,int o,int c,vector<string> &ans,string str){
+    void paranthesis(int n,int o,int c,string str,vector<string> & ans){
         if(str.size()==2*n){
             ans.push_back(str);
             return;
         }
         if(o<n){
-            func(n,o+1,c,ans,str+'(');
-
+            paranthesis(n,o+1,c,str+'(',ans);
         }
         if(c<o){
-            func(n,o,c+1,ans,str+')');
-
+            paranthesis(n,o,c+1,str+')',ans);
         }
-
     }
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        if(n==0) return ans;
-        string str="";
-        int o=0;
-        int c=0;
-        func(n,o,c,ans,str);
+        paranthesis(n,0,0,"",ans);
         return ans;
     }
 };
