f8850f588a90efb5678dc8a7cb49f907f30f99fc
Divyansh Kumawat <153420810+Divyansh-Kumawat@users.noreply.github.com>
2026-03-16 20:33:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    void find(vector<vector<int>>& ans,vector<int>& temp,vector<int> candidates,int target,int i){
+    void combination(vector<int> & candidates,int target,vector<vector<int>>& ans,vector<int>& temp,int i){
+        
+        int sum=accumulate(temp.begin(),temp.end(),0);
         if(i==candidates.size()){
-            if(target==0){
-                ans.push_back(temp);   
+            if(sum==target){
+                ans.push_back(temp);
             }
             return;
         }
-        if(target>0){
+        
+        if(sum<=target){
             temp.push_back(candidates[i]);
-            find(ans,temp,candidates,target-candidates[i],i);
+            combination(candidates,target,ans,temp,i);
             temp.pop_back();
         }
-        find(ans,temp,candidates,target,i+1);
+        combination(candidates,target,ans,temp,i+1);        
+
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> temp;
-        find(ans,temp,candidates,target,0);
+        combination(candidates,target,ans,temp,0);
         return ans;
     }
 };
